3d15285ef63b66e3bbaa5a39209bb0ba41055125
Hawk Weisman <hawk@meteorcodelabs.com>
2015-01-18 23:24:28 -0500

    Reorganize namespaces in svm

--- seax-svm/src/lib.rs
+++ seax-svm/src/lib.rs
@@ -4,37 +4,39 @@ fn it_works() {
 }
 
 
-mod SVM {
-
-    /// Singly-linked cons list.
-    ///
-    /// This is used internally to represent list primitives in the machine.
-    pub enum List<T> {
-        Nil,
-        Cons(T, Box<List<T>>)
-    }
-
-    /// Public implementation for List.
-    impl<T> List<T> {
-
-        /// Creates a new empty list
-        fn new() -> List<T> {
-            List::Nil
+pub mod svm {
+    mod slist {
+        use svm::slist::List::{Cons,Nil};
+        /// Singly-linked cons list.
+        ///
+        /// This is used internally to represent list primitives in the machine.
+        enum List<T> {
+            Nil,
+            Cons(T, Box<List<T>>)
         }
 
-        /// Prepends the given item to the list
-        fn prepend(self, it: T) -> List<T> {
-            List::Cons(it, box self)
-        }
+        /// Public implementation for List.
+        impl<T> List<T> {
 
-        /// Returns the length of the list
-        fn length (&self) -> i32 {
-            match *self {
-                List::Cons(_, ref tail) => 1 + tail.length(),
-                List::Nil => 0
+
+            /// Creates a new empty list
+            fn new() -> List<T> {
+                Nil
+            }
+
+            /// Prepends the given item to the list
+            fn prepend(self, it: T) -> List<T> {
+                Cons(it, box self)
+            }
+
+            /// Returns the length of the list
+            fn length (&self) -> i32 {
+                match *self {
+                    Cons(_, ref tail) => 1 + tail.length(),
+                    Nil => 0
+                }
             }
         }
-
     }
 
     enum Exp {
