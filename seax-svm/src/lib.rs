33224a2b88c368d890d26499a219df544644d4c2
Hawk Weisman <hawk@meteorcodelabs.com>
2015-01-18 23:08:15 -0500

    Fix issue in list

--- seax-svm/src/lib.rs
+++ seax-svm/src/lib.rs
@@ -4,19 +4,20 @@ fn it_works() {
 
 
 mod SVM {
+
     /// Singly-linked cons list.
     ///
     /// This is used internally to represent list primitives in the machine.
     pub enum List<T> {
         Nil,
-        Cons(T, List<T>)
+        Cons(T, Box<List<T>>)
     }
 
     /// Public implementation for List.
     impl<T> List<T> {
 
         fn new() -> List<T> {
-            Nil
+            List::Nil
         }
 
     }
