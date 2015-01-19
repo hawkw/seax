7aed466bf7cd7360c849d34196e83215f1db22cc
Hawk Weisman <hawk@meteorcodelabs.com>
2015-01-18 19:23:26 -0500

    Add list stuff

--- seax-svm/src/lib.rs
+++ seax-svm/src/lib.rs
@@ -2,7 +2,25 @@
 fn it_works() {
 }
 
+
 mod SVM {
+    /// Singly-linked cons list.
+    ///
+    /// This is used internally to represent list primitives in the machine.
+    pub enum List<T> {
+        Cons(T ~List<T>),
+        Nil
+    }
+
+    /// Public implementation for List.
+    pub impl List<T> {
+
+        pub fn new() -> List<T> {
+            Nil
+        }
+
+    }
+
     enum Exp {
         Number(int)
     }
