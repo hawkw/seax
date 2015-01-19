972c867cff9108a940d8654d206caf35d870704b
Hawk Weisman <hawk@meteorcodelabs.com>
2015-01-19 11:47:09 -0500

    Add to_string() for List

--- seax-svm/src/lib.rs
+++ seax-svm/src/lib.rs
@@ -6,13 +6,17 @@ fn it_works() {
 
 pub mod svm {
     mod slist {
+
         use svm::slist::List::{Cons,Nil};
+        use std::fmt::Show;
+
         /// Singly-linked cons list.
         ///
         /// This is used internally to represent list primitives in the machine.
-        enum List<T> {
+        #[deriving(Show)]
+        pub enum List<T> {
+            Cons(T, Box<List<T>>),
             Nil,
-            Cons(T, Box<List<T>>)
         }
 
         /// Public implementation for List.
@@ -35,6 +39,35 @@ pub mod svm {
                     Cons(_, ref tail) => 1 + tail.length(),
                     Nil => 0
                 }
+            }
+        }
+
+        impl<T> List<T> where T: Show {
+            /// Return a string representation of the list
+            fn to_string(&self) -> String {
+                match *self {
+                    Cons(ref head, ref tail) => format!("({:?}, {})", head, tail.to_string()),
+                    Nil => format!("nil")
+                }
+
+            }
+        }
+
+        #[cfg(test)]
+        mod tests {
+            use super::List;
+            use super::List::{Cons,Nil};
+
+            #[test]
+            fn test_length() {
+                let l: List<i32> = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+                assert_eq!(l.length(), 3);
+            }
+
+             #[test]
+            fn test_stringify() {
+                let l: List<i32> = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+                assert_eq!(l.to_string(), "(1i32, (2i32, (3i32, nil)))");
             }
         }
     }
