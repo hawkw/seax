3ce83f1de5f89286045b1e719416ddb5403a2280
Hawk Weisman <hawk@meteorcodelabs.com>
2015-02-09 12:54:43 -0500

    Implement LT and LTE

--- seax_svm/src/lib.rs
+++ seax_svm/src/lib.rs
@@ -292,6 +292,32 @@ pub mod svm {
                     (_,_) => unimplemented!()
                     }
                 },
+                InstCell(LT) => {
+                    let (op1, new_stack) = self.stack.pop().unwrap();
+                    let (op2, newer_stack) = new_stack.pop().unwrap();
+                    match (op1,op2) {
+                        (AtomCell(a), AtomCell(b)) => State {
+                            stack: newer_stack.push(AtomCell(Bool(a < b))),
+                            env: self.env,
+                            control: new_control,
+                            dump: self.dump
+                        },
+                    (_,_) => unimplemented!()
+                    }
+                },
+                InstCell(LTE) => {
+                    let (op1, new_stack) = self.stack.pop().unwrap();
+                    let (op2, newer_stack) = new_stack.pop().unwrap();
+                    match (op1,op2) {
+                        (AtomCell(a), AtomCell(b)) => State {
+                            stack: newer_stack.push(AtomCell(Bool(a <= b))),
+                            env: self.env,
+                            control: new_control,
+                            dump: self.dump
+                        },
+                    (_,_) => unimplemented!()
+                    }
+                },
                 _ => { unimplemented!() }
             }
         }
