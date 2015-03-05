c7c77a87c157381efe1318bcfbbb69078c9c188c
Hawk Weisman <hawk@meteorcodelabs.com>
2015-03-04 20:35:24 -0500

    Fix unclosed string in publish.sh

--- .travis/publish.sh
+++ .travis/publish.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ "$TRAVIS_REPO_SLUG" == "hawkw/seax ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
+if [ "$TRAVIS_REPO_SLUG" == "hawkw/seax" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
 
 	echo -e "Publishing RustDoc...\n"
 
