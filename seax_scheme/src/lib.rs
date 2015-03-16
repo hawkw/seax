d31e3c330f36e830f83686bd64b6f509ad77eac7
Hawk Weisman <hawk@meteorcodelabs.com>
2015-03-16 19:04:02 -0400

    Start on main library module

--- seax_scheme/src/lib.rs
+++ seax_scheme/src/lib.rs
@@ -1,8 +1,4 @@
-#[macro_use]
-extern crate nom;
-
-use nom::{IResult,Needed,FlatMapOpt,line_ending,not_line_ending, space, alphanumeric, multispace};
-use nom::IResult::*;
-
-use std::str;
-use std::collections::HashMap;
+#![crate_name = "seax_scheme"]
+#![crate_type = "lib"]
+pub mod ast;
+pub mod parser;
