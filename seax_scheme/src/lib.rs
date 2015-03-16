4eb5adcbaeda6d81ae996bed892bae8c0104fbdf
Hawk Weisman <hawk@meteorcodelabs.com>
2015-03-16 19:01:59 -0400

    Move parsing stuff to parser file

--- seax_scheme/src/lib.rs
+++ seax_scheme/src/lib.rs
@@ -1,2 +1,8 @@
 #[macro_use]
 extern crate nom;
+
+use nom::{IResult,Needed,FlatMapOpt,line_ending,not_line_ending, space, alphanumeric, multispace};
+use nom::IResult::*;
+
+use std::str;
+use std::collections::HashMap;
---
+++ seax_scheme/src/parser.rs
@@ -0,0 +1,8 @@
+#[macro_use]
+extern crate nom;
+
+use nom::{IResult,FlatMapOpt,line_ending,not_line_ending, space, alphanumeric, multispace};
+use nom::IResult::*;
+
+use std::str;
+use std::collections::HashMap;
