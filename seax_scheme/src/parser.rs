4980a544bed1f6a8489a57a0eb45799eaf9d7737
Hawk Weisman <hawk@meteorcodelabs.com>
2015-03-17 17:32:50 -0400

    Fix tests for parser panicking

--- seax_scheme/src/parser.rs
+++ seax_scheme/src/parser.rs
@@ -1,17 +1,18 @@
 extern crate "parser-combinators" as parser_combinators;
 use self::parser_combinators::{between, spaces, many1, sep_by, satisfy, Parser, ParserExt, ParseResult};
 use self::parser_combinators::primitives::{State, Stream};
-use ast;
+use super::ast;
 
 
-fn expr<I>(input: State<I>) -> ParseResult(ast::ExprNode, I)
+pub fn expr<I>(input: State<I>) -> ParseResult(ast::ExprNode, I)
     where I: Stream<Item=char> {
         unimplemented!()
 }
 
 #[cfg(test)]
 mod tests {
-
+    use ::ast;
+    use super::expr;
     #[test]
     fn test_basic_ident() {
         assert_eq!(
