147afbd2dcbc818b6f2e3a81bb7817b9d86e5ed5
Hawk Weisman <hawk@meteorcodelabs.com>
2015-03-17 17:23:42 -0400

    Start on parsing with parser_combinators

--- seax_scheme/src/parser.rs
+++ seax_scheme/src/parser.rs
@@ -3,3 +3,21 @@ use self::parser_combinators::{between, spaces, many1, sep_by, satisfy, Parser, 
 use self::parser_combinators::primitives::{State, Stream};
 use ast;
 
+
+fn expr<I>(input: State<I>) -> ParseResult(ast::ExprNode, I)
+    where I: Stream<Item=char> {
+        unimplemented!()
+}
+
+#[cfg(test)]
+mod tests {
+
+    #[test]
+    fn test_basic_ident() {
+        assert_eq!(
+            (expr as fn (_) -> _).parse("ident"),
+            Ok((ast::NameNode { name: "ident" }, ""))
+            );
+    }
+
+}
