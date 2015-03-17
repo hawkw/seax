fae717bfa9985578a4005becc822a3eb3de654e5
Hawk Weisman <hawk@meteorcodelabs.com>
2015-03-17 18:18:54 -0400

    Basic parser for names

--- seax_scheme/src/parser.rs
+++ seax_scheme/src/parser.rs
@@ -1,23 +1,38 @@
 extern crate "parser-combinators" as parser_combinators;
-use self::parser_combinators::{between, spaces, many1, sep_by, satisfy, Parser, ParserExt, ParseResult};
+use self::parser_combinators::{between, spaces, many, many1, sep_by, satisfy, string,Parser, ParserExt, ParseResult};
 use self::parser_combinators::primitives::{State, Stream};
 use super::ast;
+use super::ast::ExprNode;
 
 
-pub fn expr<I>(input: State<I>) -> ParseResult(ast::ExprNode, I)
+pub fn expr<I>(input: State<I>) -> ParseResult<ast::ExprNode, I>
     where I: Stream<Item=char> {
-        unimplemented!()
+
+        let spaces = spaces();
+        let name = many1(satisfy(|c| c.is_alphabetic()));
+        /* // TODO: fix
+        let sexpr = between(
+            satisfy(|c| c == '('),
+            satisfy(|c| c == ')'),
+            expr as fn (_) -> _
+            );*/
+        spaces.clone().with(
+             name.map(|it: String| ast::ExprNode::Name(ast::NameNode{ name: it}))
+             //.or(sexpr.map(|it| ast::ExprNode::SExpr(ast::SExprNode())))
+         ).parse_state(input)
 }
 
 #[cfg(test)]
 mod tests {
     use ::ast;
     use super::expr;
+    use super::parser_combinators::{Parser, ParseResult};
+
     #[test]
     fn test_basic_ident() {
         assert_eq!(
-            (expr as fn (_) -> _).parse("ident"),
-            Ok((ast::NameNode { name: "ident" }, ""))
+            (expr as fn (_) -> _).parse("ident").unwrap(),
+            (ast::ExprNode::Name(ast::NameNode { name: "ident".to_string() }), "")
             );
     }
 
