8f3b0b8c6a3a53b00120a418c765cda9a8955649
Hawk Weisman <hawk@meteorcodelabs.com>
2015-03-17 16:26:52 -0400

    Additional work on AST

--- seax_scheme/src/ast.rs
+++ seax_scheme/src/ast.rs
@@ -1,14 +1,18 @@
-extern crate seax_svm;
-
 use seax_svm::svm::cell::SVMCell;
+use seax_svm::slist::List;
 
 /// Trait for AST nodes.
-pub trait SyntaxTree {
+pub trait ASTNode {
     /// Compile this node to a list of SVM expressions
-    fn compile(self)                    -> Vec<SVMCell>;
+    fn compile(self)                        -> List<SVMCell>;
+
+    /// Pretty-print this node
+    fn prettyprint(&self)                   -> String {
+        prettyprint_level(&self, 0usize)
+    }
 
     /// Pretty-print this node at the desired indent level
-    fn prettyprint(&self, level: usize) -> String;
+    fn print_level(&self, level: usize)     -> String;
 }
 
 /// Expression
@@ -16,18 +20,68 @@ pub trait SyntaxTree {
 /// All Seax Scheme expressions are one of the following
 ///  + Nested S-Expressions
 ///  + Identifiers
+///  + Lists
 ///  + Numbers
 ///     - signed int
 ///     - unsigned int
-///     - float
+///     - floating-point
 ///  + Characters
 ///  + Strings
-pub enum Expr {
-    SExpr(List<Expr>),
-    Ident(String),
-    SInt(isize),
-    UInt(usize),
-    Float(f64),
-    Char(char),
-    String(String)
+///  TODO: implement the entire Scheme 'numeric tower'
+///  TODO: macros should happen
+#[deriving(Clone, PartialEq)]
+pub enum ExprNode {
+    Root(RootNode),
+    SExpr(SExprNode),
+    Name(NameNode),
+    List(ListNode),
+    IntConst(IntNode),
+    UIntConst(UIntNode),
+    FloatConst(FloatNode),
+    BoolConst(BoolNode),
+    StringConst(StringNode),
+    CharConst(CharNode),
 }
+
+/// AST node for the root of a program's AST
+#[deriving(Clone, PartialEq)]
+pub struct RootNode { pub exprs: Vec<ExprNode> }
+
+/// AST node for an S-expression.
+///
+/// This includes function application, assignment,
+/// function definition, et cetera...Scheme is not a complexl anguage.
+#[deriving(Clone, PartialEq)]
+pub struct SExprNode {
+    pub operator: NameNode,
+    pub operands: Vec<ExprNode>,
+}
+
+/// AST node for a list literal
+#[deriving(Clone, PartialEq)]
+pub struct ListNode { pub elements: Vec<ExprNode> }
+
+/// AST node for an identifier
+#[deriving(Clone, PartialEq)]
+pub struct NameNode { pub name: String }
+
+/// AST node for an integer constant
+#[deriving(Clone, PartialEq)]
+pub struct IntNode { pub value: isize }
+
+/// AST node for an unsigned integer constant
+#[deriving(Clone, PartialEq)]
+pub struct UIntNode { pub value: usize }
+
+/// AST node for a floating-point constant
+#[deriving(Clone, PartialEq)]
+pub struct FloatNode { pub value: f64 }
+
+/// AST node for a boolean constant
+#[deriving(Clone, PartialEq)]
+pub struct BoolNode { pub value: bool }
+
+/// AST node for a character constant
+#[deriving(Clone, PartialEq)]
+pub struct CharNode { pub value: char }
+
