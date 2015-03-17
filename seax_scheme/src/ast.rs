3177344bcb96394da8f107316f3f5ef5fbc9f221
Hawk Weisman <hawk@meteorcodelabs.com>
2015-03-17 16:33:59 -0400

    Use correct name for 'derive'

--- seax_scheme/src/ast.rs
+++ seax_scheme/src/ast.rs
@@ -29,7 +29,7 @@ pub trait ASTNode {
 ///  + Strings
 ///  TODO: implement the entire Scheme 'numeric tower'
 ///  TODO: macros should happen
-#[deriving(Clone, PartialEq)]
+#[derive(Clone, PartialEq)]
 pub enum ExprNode {
     Root(RootNode),
     SExpr(SExprNode),
@@ -44,44 +44,44 @@ pub enum ExprNode {
 }
 
 /// AST node for the root of a program's AST
-#[deriving(Clone, PartialEq)]
+#[derive(Clone, PartialEq)]
 pub struct RootNode { pub exprs: Vec<ExprNode> }
 
 /// AST node for an S-expression.
 ///
 /// This includes function application, assignment,
 /// function definition, et cetera...Scheme is not a complexl anguage.
-#[deriving(Clone, PartialEq)]
+#[derive(Clone, PartialEq)]
 pub struct SExprNode {
     pub operator: NameNode,
     pub operands: Vec<ExprNode>,
 }
 
 /// AST node for a list literal
-#[deriving(Clone, PartialEq)]
+#[derive(Clone, PartialEq)]
 pub struct ListNode { pub elements: Vec<ExprNode> }
 
 /// AST node for an identifier
-#[deriving(Clone, PartialEq)]
+#[derive(Clone, PartialEq)]
 pub struct NameNode { pub name: String }
 
 /// AST node for an integer constant
-#[deriving(Clone, PartialEq)]
+#[derive(Clone, PartialEq)]
 pub struct IntNode { pub value: isize }
 
 /// AST node for an unsigned integer constant
-#[deriving(Clone, PartialEq)]
+#[derive(Clone, PartialEq)]
 pub struct UIntNode { pub value: usize }
 
 /// AST node for a floating-point constant
-#[deriving(Clone, PartialEq)]
+#[derive(Clone, PartialEq)]
 pub struct FloatNode { pub value: f64 }
 
 /// AST node for a boolean constant
-#[deriving(Clone, PartialEq)]
+#[derive(Clone, PartialEq)]
 pub struct BoolNode { pub value: bool }
 
 /// AST node for a character constant
-#[deriving(Clone, PartialEq)]
+#[derive(Clone, PartialEq)]
 pub struct CharNode { pub value: char }
 
