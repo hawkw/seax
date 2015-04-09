3fdba944411c1ffdb16429d907a186ecab1dbde1
travis-ci <travis@travis-ci.org>
2015-04-09 18:55:50 +0000

    Lastest RustDoc for version svm-v0.2.3 on successful travis build 272 auto-pushed to gh-pages

---
+++ api/implementors/core/iter/trait.FromIterator.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['seax_svm'] = ["<a class='stability Stable' title='Stable'></a>impl&lt;T&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/iter/trait.FromIterator.html' title='core::iter::FromIterator'>FromIterator</a>&lt;T&gt; for <a class='enum' href='seax_svm/slist/enum.List.html' title='seax_svm::slist::List'>List</a>&lt;T&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
