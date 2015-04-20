df5e8ba8b9d81dfa5ef68f8d1be6475422c9e966
Hawk Weisman <hawk@meteorcodelabs.com>
2015-04-19 20:21:49 -0400

    Started on REPL
    needs work

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,52 @@
+#![feature(scheme)]
+#![feature(compile)]
+#![feature(convert)]
 extern crate rustc_serialize;
 extern crate docopt;
 
+extern crate seax_svm as svm;
+extern crate seax_scheme as scheme;
+
 use docopt::Docopt;
+use std::io;
+use std::io::{Read, BufRead};
 
 static USAGE: &'static str = "
-Usage: seax [-v] <bin>
+Usage:
+    seax repl [-vd]
+    seax [-vd] <bin>
 
 Options:
     -v, --verbose   Enable verbose mode
+    -d, --debug     Enable debug mode
 ";
 
-#[derive(RustcDecodable, Debug)]
+#[derive(RustcDecodable)]
 struct Args {
+    cmd_repl: bool,
     arg_bin: String,
-    flag_verbose: bool
+    flag_verbose: bool,
+    flag_debug: bool,
 }
+
 
 fn main() {
     let args: Args = Docopt::new(USAGE)
-                            .and_then(|d| d.decode())
-                            .unwrap_or_else(|e| e.exit());
-    println!("{:?}", args);
+                .and_then(|d| d.decode())
+                .unwrap_or_else(|e| e.exit());
+    if args.cmd_repl {
+        let mut console = io::stdin();
+        let lock = console.lock();
+        for line in lock.lines() {
+            println!(">");
+            match line {
+                Ok(line) => { scheme::compile(line.as_ref())
+                                .and_then(  |p| Ok(svm::eval_program(p, args.flag_debug)) )
+                                .map(       |r| println!("{}", r) )
+                                .map_err(   |e| println!("{}", e) ); },
+                Err(why) => println!("{}", why)
+
+            }
+        }
+    }
 }
